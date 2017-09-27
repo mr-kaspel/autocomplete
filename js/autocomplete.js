e15f8f56d0dafeee6d142ad94bcc5eeb429d652b
Andrew k-mp <EndryNiksan@yandex.com>
2017-09-27 21:37:17 +0500

    Corrected mistakes. Relatively works in faryf, error on event

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const STYLE_BORDER_BLUE = '2px solid #5199db';
 const STYLE_BORDER_RED = '2px solid #e3071c';
@@ -21,19 +21,20 @@ function citySearch(city){
 
 	var argument = document.getElementById('autocomplete');
 	var agr = argument.value;
-	list.innerHTML = '';
+	document.getElementById('list').innerHTML = '';
 
 		city.forEach(function(city){
 			var clip = city.City.slice(0, agr.length);
 			var licouter = document.getElementById('list').getElementsByTagName('li').length;
 
-			if(clip.match(eval('{' + ('/' + agr + '/gi') + '}')) != null){
+			if(clip.match(eval('{' + ('/' + agr + '/gi') + '}')) !== null){
+				
 				if(licouter <= 4 && agr.length <= 2){
-					var li = list.appendChild(document.createElement('li'));
+					var li = document.getElementById('list').appendChild(document.createElement('li'));
 					li.innerHTML = city.City;
 
 				}else if ( agr.length > 2 ){
-					var li = list.appendChild(document.createElement('li'));
+					li = document.getElementById('list').appendChild(document.createElement('li'));
 					li.innerHTML = city.City;
 				}
 			}
@@ -53,14 +54,14 @@ function loadDocument(){
 		if (xhr.readyState != 4) return;
 
 		if (xhr.status != 200){
-			list.innerHTML = '<li>Что-то пошло не так. Проверьте соединение с интернетом и попробуйте еще раз</li>' + '<li tabindex="1">Обновить</li>';
+			document.getElementById('list').innerHTML = '<li>Что-то пошло не так. Проверьте соединение с интернетом и попробуйте еще раз</li>' + '<li tabindex="1">Обновить</li>';
 			document.getElementById("list").style.display = 'block';
 			document.getElementById("autocomplete").style.border = STYLE_BORDER_RED;
 		}else{
 			try{
 				var city = JSON.parse(xhr.responseText);
 			}catch (e){
-				list.innerHTML = '<li>Некорректный ответ' + e.message + '</li>';
+				document.getElementById('list').innerHTML = '<li>Некорректный ответ' + e.message + '</li>';
 			}
 			// делаем задержку, в любом случае секунду
 			setTimeout(function() {
@@ -68,14 +69,14 @@ function loadDocument(){
 			}, 1000);
 			
 		}
-	}
+	};
 }
 
 function loadCity(){
 
 	loadDocument();
 
-	list.innerHTML = '<li class="inf"><div class="load"></div>&emsp;Загрузка</li>';
+	document.getElementById('list').innerHTML = '<li class="inf"><div class="load"></div>&emsp;Загрузка</li>';
 	document.getElementById("list").style.display = 'block';
 	document.getElementById("list").style.height = '30px';
 }
@@ -87,10 +88,10 @@ function showCity(city){
 	var licouter = document.getElementById('list').getElementsByTagName('li').length;
 
 	for(var i = 0; i < licouter; i++){
-		list.childNodes[i].tabIndex = i;
+		document.getElementById('list').childNodes[i].tabIndex = i;
 	}
 
-	for(var i = 1; i <= 4; i++){
+	for(i = 1; i <= 4; i++){
 		if(licouter == i ){
 			var x = i * 30;
 			document.getElementById("list").style.height = x + 'px';
@@ -99,24 +100,24 @@ function showCity(city){
 	}
 
 	if(licouter == 5){
-		list.appendChild(document.createElement('li')).innerHTML = '<p>Показано 5 найденных городов. <br> Уточните запрос, чтобы увидеть остальные</p>';
+		document.getElementById('list').appendChild(document.createElement('li')).innerHTML = '<p>Показано 5 найденных городов. <br> Уточните запрос, чтобы увидеть остальные</p>';
 	}
 
 	if (licouter > 4){
 		document.getElementById("list").style.height = '200px';
 		document.getElementById("list").style.display = 'block';
-	}else if(licouter == 0 || event.keyCode == '8'){
-		list.innerHTML = ' <li><p>Совпадений не найдено</p></li>';
+	}else if(licouter === 0 || event.keyCode == '8'){
+		document.getElementById('list').innerHTML = ' <li><p>Совпадений не найдено</p></li>';
 	}
 		if(licouter > 0){
 		document.getElementById('list').childNodes[0].style.background = '#5199db';
 		document.getElementById('list').childNodes[0].style.color = '#fff';
 	}
 	
-	autocomplete.addEventListener('keydown', function(){
+	document.getElementById('autocomplete').addEventListener('keydown', function(){
 		if(licouter > 0 && event.keyCode == 13 && document.getElementById('autocomplete').value.length > 0){
 			document.getElementById('autocomplete').value = document.getElementById('list').childNodes[0].textContent;
-			list.style.display = 'none';
+			document.getElementById('list').style.display = 'none';
 			event.preventDefault();
 		}
 	});
@@ -124,9 +125,9 @@ function showCity(city){
 
 var numeric = ["81", "87", "69", "82", "84", "89", "85", "73", "79", "80" ,"219" ,"221", "65", "83", "68", "70", "71", "72", "74", "75", "76", "186", "222", "90", "88", "67", "86", "66", "78", "77", "188", "190", "191", "192", "8", "49", "50", "51", "52", "53", "54", "55", "56", "57", "48"];
 
-autocomplete.addEventListener('keydown', function(){
+document.getElementById('autocomplete').addEventListener('keydown', function(event){
 	document.getElementById("autocomplete").style.border = '';
-	eror.innerHTML = '';
+	document.getElementById('eror').innerHTML = '';
 
 	for(var i = 0; i <= numeric.length; i++){
 		if ( event.keyCode == numeric[i] ) {
@@ -144,7 +145,7 @@ autocomplete.addEventListener('keydown', function(){
 
 	var licouter = document.getElementById('list').getElementsByTagName('li').length;
 
-	if (event.keyCode == 40 && licouter != 0){
+	if (event.keyCode == 40 && licouter !== 0){
 		document.getElementById('list').childNodes[1].focus();
 		document.getElementById("autocomplete").style.border = STYLE_BORDER_BLUE;
 		document.getElementById('list').childNodes[0].style.background = '';
@@ -153,19 +154,17 @@ autocomplete.addEventListener('keydown', function(){
 	}
 });
 
-autocomplete.addEventListener('focus', function(){
+document.getElementById('autocomplete').addEventListener('focus', function(){
 	document.getElementById("autocomplete").style.border = STYLE_BORDER_BLUE;
 
 });
 
-autocomplete.addEventListener('mouseover', function(){
-	ymaps.ready(function() {
-		var youcity = ymaps.geolocation.city;
-		autocomplete.placeholder = "Возможно " + String(youcity);
-	});
+document.getElementById('autocomplete').addEventListener('mouseover', function(){
+	var youcity = ymaps.geolocation.city;
+	document.getElementById('autocomplete').placeholder = "Возможно " + String(youcity);
 });
 
-list.addEventListener('keydown', function(){
+document.getElementById('list').addEventListener('keydown', function(){
 
 	var numActiveItem = document.activeElement.tabIndex;
 
@@ -183,7 +182,7 @@ list.addEventListener('keydown', function(){
 		case 13:
 		event.preventDefault();
 		document.getElementById('autocomplete').value = event.target.textContent;
-		autocomplete.focus();
+		document.getElementById('autocomplete').focus();
 		document.getElementById("autocomplete").style.border = '';
 		document.getElementById('list').style.display = 'none';
 		break;
@@ -199,18 +198,18 @@ list.addEventListener('keydown', function(){
 	}
 });
 
-list.addEventListener('click', function(){
+document.getElementById('list').addEventListener('click', function(){
 	document.getElementById('autocomplete').value = event.target.textContent;
-	autocomplete.focus();
+	document.getElementById('autocomplete').focus();
 	document.getElementById("autocomplete").style.border = '';
 	document.getElementById("list").style.display = 'none';
 });
 
-list.addEventListener('mouseover', function(){
+document.getElementById('list').addEventListener('mouseover', function(){
 	fixTheScroll();
 });
 
-list.addEventListener('mouseleave', function(){
+document.getElementById('list').addEventListener('mouseleave', function(){
 	unlockTheScroll();
 });
 
@@ -225,9 +224,8 @@ document.body.addEventListener('click', function(){
 	function checkSelected(){
 		for(var i = 0; i < licouter; ++i){
 			if(document.getElementById('autocomplete').value.toLowerCase() == document.getElementById('list').childNodes[i].textContent.toLowerCase()){
-				list.style.display = 'none';
+				document.getElementById('list').style.display = 'none';
 				return true;
-				break;
 			}
 		}
 		return false;
@@ -235,16 +233,16 @@ document.body.addEventListener('click', function(){
 
 	if(licouter > 0){
 		if(checkSelected()){
-			list.style.display = 'none';
+			document.getElementById('list').style.display = 'none';
 		}else{
-			eror.innerHTML = 'Выберите значение из списка';
-			list.style.display = 'none';
-			autocomplete.style.border = STYLE_BORDER_RED;
+			document.getElementById('eror').innerHTML = 'Выберите значение из списка';
+			document.getElementById('list').style.display = 'none';
+			document.getElementById('autocomplete').style.border = STYLE_BORDER_RED;
 		}
 	}
 });
 
-autocomplete.addEventListener('blur', function(){
+document.getElementById('autocomplete').addEventListener('blur', function(){
 
 	document.getElementById("autocomplete").style.border = '';
 
